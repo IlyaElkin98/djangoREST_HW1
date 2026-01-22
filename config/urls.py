ca1edfcf75806df76a6c89ae757a19d373da0e97
ilya <ilifud98@mail.ru>
2026-01-23 00:40:12 +0700

    refactor(config.urls): add path users & materials

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('materials.urls', namespace='materials')),
-    path("users", include('user.urls', namespace='users')),
 ]
