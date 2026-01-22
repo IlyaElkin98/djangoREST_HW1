3b9c34d79340e7d960f941a159ea812e55bb7635
ilya <ilifud98@mail.ru>
2026-01-23 00:42:51 +0700

    refactor(materials.views): add views.py(generic & viewset)

--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
+from .models import Lesson, Course
+from .serializers import LessonSerializer, CourseSerializer
 
-# Create your views here.
+
+# CRUD для модели "Course" (View-sets)
+class CourseViewSet(viewsets.ModelViewSet):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
+
+# CRUD для модели "Lesson" (Generic-классы)
+class LessonCreateAPIView(generics.CreateAPIView):
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(generics.ListAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonDestroyAPIView(generics.DestroyAPIView):
+    queryset = Lesson.objects.all()
