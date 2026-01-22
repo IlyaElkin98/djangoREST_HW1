4121ff3c725057d7eb2f824cce6fe3f56fca1bb9
ilya <ilifud98@mail.ru>
2026-01-23 00:42:10 +0700

    refactor(materials.models): add models.py + migrate

--- materials/models.py
+++ materials/models.py
@@ -3,7 +3,7 @@ from users.models import User
 
 class Course(models.Model):
     name = models.CharField(max_length=200)
-    preview_image = models.ImageField(upload_to='courses/previews/')
+    preview_image = models.URLField()
     description = models.TextField()
 
     def __str__(self):
@@ -13,7 +13,7 @@ class Lesson(models.Model):
     course = models.ForeignKey(Course, related_name='lessons', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
-    preview_image = models.ImageField(upload_to='lessons/previews/')
+    preview_image = models.URLField()
     video_url = models.URLField()
 
     def __str__(self):
