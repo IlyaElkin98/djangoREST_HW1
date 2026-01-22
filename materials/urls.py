2be4318817769a89b8c570fdba87896d66f752e2
ilya <ilifud98@mail.ru>
2026-01-23 00:41:27 +0700

    refactor(materials.urls): add urls.py

---
+++ materials/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework.urls import app_name
+
+from .apps import MaterialsConfig
+from .views import LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, LessonUpdateAPIView, \
+    LessonDestroyAPIView, CourseViewSet
+
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet, basename='course')
+
+app_name = MaterialsConfig.name
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('lesson/new/', LessonCreateAPIView.as_view(), name='lesson-create'),
+    path('lessons/', LessonListAPIView.as_view(), name='lesson-list'),
+    path('lessons/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-get'),
+    path('lessons/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'),
+    path('lessons/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
+] + router.urls
