411a8bab7a3b81c7bc8143e17e077b54dae5f811
ilya <ilifud98@mail.ru>
2026-01-23 22:26:25 +0700

    refactor(materials.serializers): edit serializers.py

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
+
 from .models import Course, Lesson
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -11,3 +13,14 @@ class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class CourseCountSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+
+    def get_lesson_count(self, obj):
+        return obj.course.count()
+
+    class Meta:
+        model = Course
+        fields = ('name', 'description',)
