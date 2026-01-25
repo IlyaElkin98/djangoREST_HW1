427f7ac4624633d0142ff3e9f0cfb34422ee7598
ilya <ilifud98@mail.ru>
2026-01-25 23:01:13 +0700

    refactor(materials.models): edit models.py + migrate

--- materials/models.py
+++ materials/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from users.models import User
+
 
 class Course(models.Model):
     name = models.CharField(max_length=200)
-    preview_image = models.URLField()
+    preview_image = models.ImageField(upload_to='materials/preview', null=True, blank=True)
     description = models.TextField()
 
     def __str__(self):
@@ -13,7 +13,7 @@ class Lesson(models.Model):
     course = models.ForeignKey(Course, related_name='lessons', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
-    preview_image = models.URLField()
+    preview_image = models.ImageField(upload_to='materials/preview', null=True, blank=True)
     video_url = models.URLField()
 
     def __str__(self):
