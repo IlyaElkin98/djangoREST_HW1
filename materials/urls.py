fa71bb98a04bdfb4bf9c90ad42c7d72d065886f4
ilya <ilifud98@mail.ru>
2026-02-03 21:30:10 +0700

    refactor(urls): Task2: add new path

--- materials/urls.py
+++ materials/urls.py
@@ -1,23 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework.urls import app_name
-
-from .apps import MaterialsConfig
-from .views import LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, LessonUpdateAPIView, \
-    LessonDestroyAPIView, CourseViewSet, CourseListView, PaymentListView
+from materials.apps import MaterialsConfig
+from .views import CourseViewSet, CourseListView, PaymentListView, LessonViewSet
 
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='course')
+router.register(r'lesson', LessonViewSet, basename='lesson')
 
 app_name = MaterialsConfig.name
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('lesson/new/', LessonCreateAPIView.as_view(), name='lesson-create'),
-    path('lessons/', LessonListAPIView.as_view(), name='lesson-list'),
-    path('lessons/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-get'),
-    path('lessons/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'),
-    path('lessons/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
 
     path('courses_detail/', CourseListView.as_view(), name='courses-detail'),
     path('payment_list/', PaymentListView.as_view(), name='payment-list'),
