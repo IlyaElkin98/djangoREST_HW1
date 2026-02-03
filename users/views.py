fe7d3438636f44e25804c9d6a98cb8cb65f273d0
ilya <ilifud98@mail.ru>
2026-02-03 18:23:45 +0700

    refactor(users.models): Task1: add view (CRUD auth and register not auth)

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Закрыть экшены авторизацией, кроме регистрации.
+
+    def get_permissions(self):
+        if self.action in ['create']:
+            self.permission_classes = [permissions.AllowAny]  # Разрешить регистрацию для всех
+        return super().get_permissions()
+
