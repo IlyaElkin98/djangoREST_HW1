7114062a3030db4c6cc0b203eb07074fe7d185a9
ilya <ilifud98@mail.ru>
2026-02-06 18:53:19 +0700

    refactor(materials.models): Task2: edit models.py(add model Subscription) + migrate

--- materials/models.py
+++ materials/models.py
@@ -23,3 +23,13 @@ class Lesson(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Subscription(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Название подписки")
+    user = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, related_name='course', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
