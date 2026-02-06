e8ab901926f7b512c054db3f8cf116d489fc4541
ilya <ilifud98@mail.ru>
2026-02-06 18:44:18 +0700

    refactor(materials.views): Task1: edin models.py (add in cls Course field 'video_url') + migrate

--- materials/models.py
+++ materials/models.py
@@ -8,6 +8,7 @@ class Course(models.Model):
     preview_image = models.ImageField(upload_to='materials/preview', null=True, blank=True)
     description = models.TextField()
     owner = models.ForeignKey(User, related_name='course', on_delete=models.CASCADE, null=True, blank=True)
+    video_url = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return self.name
